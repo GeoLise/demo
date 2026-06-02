0b0720da54e6bedcb2e217a8a759271a0189b844
GeoLise <jora.liseenko@yandex.ru>
2026-06-02 21:47:35 +0300

    api, client api, queryClient

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,27 +1,19 @@
-import Image from "next/image";
-import { api } from "~/server/api";
+"use client";
 
-export default async function Home() {
-  const products = (await api.products.get()).data;
+import { useQuery } from "@tanstack/react-query";
+import { api } from "../lib/client/api";
+
+export default function Products() {
+  const { data: products, isLoading } = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      return (await api.products.get()).data;
+    },
+  });
 
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className="flex flex-col gap-4 text-red-500 text-4xl">
-          {products?.map((product) => (
-            <p key={product.id} className="!text-red-500">
-              {product.name}
-            </p>
-          ))}
-        </div>
         <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
           <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
             To get started, edit the page.tsx file.
@@ -44,22 +36,17 @@ export default async function Home() {
             center.
           </p>
         </div>
+        {isLoading && (
+          <p className="text-4xl text-green-600">Загрузка продуктов...</p>
+        )}
+        <div className="flex flex-col gap-2">
+          {products?.map((prod) => (
+            <p key={prod.id} className="text-2xl text-red-600">
+              {prod.name}
+            </p>
+          ))}
+        </div>
         <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
           <a
             className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
--- src/server/api/index.ts
+++ src/server/api/index.ts
@@ -1,6 +1,7 @@
 import Elysia from "elysia";
 import { productsRouter } from "./routers/products";
 import { categoriesRouter } from "./routers/categories";
+import { treaty } from "@elysiajs/eden";
 
 export const app = new Elysia({
   prefix: "/api",
@@ -13,3 +14,7 @@ export const app = new Elysia({
   .get("/test", () => {
     return "test test 123";
   });
+
+export const api = treaty(app).api;
+
+export type App = typeof app;
